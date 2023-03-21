c10ca3a2d1ff029d11eb9de7b87ab403ca63405b
lourenxx <guilhermepxl7@gmail.com>
2023-03-21 19:06:04 -0300

    atualização do login

--- recursos_externos.py
+++ recursos_externos.py
@@ -1,8 +1,8 @@
 from openpyxl import Workbook, load_workbook
 
 # Informações de Login
-senha = '13082021'
-usuario = 'lourenxx_g'
+senha = 'JSA18012007'
+usuario = 'metamorfoseducacao'
 
 mensagem = """Olá, bom dia! Sou Guilherme Pexirile Lourenço da Colleman The World School. Estamos lançando uma MENTORIA 100% ONLINE sobre EDUCAÇÃO 4.0, caso haja interesse nos mande uma mensagem inbox! Fazemos valores para a divulgação da mentoria em seu perfil e com o cupom GUILHERMEDESC vc garante o valor promocional para ter acesso ao curso mais IMERSIVO sobre a nova educação no Brasil! Obrigado!"""""
 
