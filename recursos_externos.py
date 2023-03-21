a33a4f83803ad2042bfdd5ff373509db51897933
lourenxx <guilhermepxl7@gmail.com>
2023-03-21 15:38:58 -0300

    adicionando a varivel usuarios para armazenar a string da send_keys() da linha 17 de automação_vscode_v2

--- recursos_externos.py
+++ recursos_externos.py
@@ -1,5 +1,6 @@
+# Informações de Login
 senha = 'JSA18012007'
-
+usuario = 'metamorfoseducacao'
 
 mensagem = """Olá, bom dia! Sou Guilherme Pexirile Lourenço da Colleman The World School. Estamos lançando uma MENTORIA 100% ONLINE sobre EDUCAÇÃO 4.0, caso haja interesse nos mande uma mensagem inbox! Fazemos valores para a divulgação da mentoria em seu perfil e com o cupom GUILHERMEDESC vc garante o valor promocional para ter acesso ao curso mais IMERSIVO sobre a nova educação no Brasil! Obrigado!"""""
 
