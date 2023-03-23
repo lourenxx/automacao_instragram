c7f8e51bc62d1302eb1d6cb932491f25113400b4
lourenxx <guilhermepxl7@gmail.com>
2023-03-22 21:33:12 -0300

    alteração para enviar para gabi

--- recursos_externos.py
+++ recursos_externos.py
@@ -1,14 +1,14 @@
 #from openpyxl import Workbook, load_workbook
 
 # Informações de Login
-senha = 'JSA18012007'
-usuario = 'metamorfoseducacao'
+senha = 'senha'
+usuario = 'nome do perfil'
 
-mensagem = """Olá, bom dia! Sou Guilherme Pexirile Lourenço da Colleman The World School. Estamos lançando uma MENTORIA 100% ONLINE sobre EDUCAÇÃO 4.0, caso haja interesse nos mande uma mensagem inbox! Fazemos valores para a divulgação da mentoria em seu perfil e com o cupom GUILHERMEDESC vc garante o valor promocional para ter acesso ao curso mais IMERSIVO sobre a nova educação no Brasil! Obrigado!"""""
+mensagem = """mensagem"""""
 
 
 
-contas = []
+contas = ['nome do perfil', 'nome do perfil', 'nome do perfil']
 
 
 
